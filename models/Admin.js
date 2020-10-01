f5f762247f4cedfe9e0a4d260d91483b0892dac0
bslzz <bishalkoid@gmail.com>
2020-10-01 11:35:49 +0300

    created admin model schema

---
+++ models/Admin.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+adminSchema.plugin(AutoIncrement, { inc_field: 'admin_id' });
+const Admin = mongoose.model('Admin', adminSchema);
+module.exports = Admin;
