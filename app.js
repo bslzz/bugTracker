708734fe8a37170cc952e7ce60c9d3fb442acc55
bslzz <bishalkoid@gmail.com>
2020-10-01 10:41:31 +0300

    added and initiated mongodb to the server

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 
+//initiate
 const app = express();
+
+//db
+require('./db/db');
 
 app.get('/', (req, res) => {
   res.json('Hello');
 });
 
+//PORT
 const PORT = process.env.PORT || 7070;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
