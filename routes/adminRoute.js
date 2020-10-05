b8591385f78771f12c227472f2a1c34a4710ebb2
bslzz <bishalkoid@gmail.com>
2020-10-05 21:40:51 +0300

    added routes to respective admin bugs

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 const adminController = require('../controllers/admin');
+const checkAuth = require('../middleware/Auth');
 
 //get Admin
 router.route('/').get(adminController.getAdmin);
 
 //register Admin
 router.route('/register').post(adminController.adminRegister);
+
+//get all bugs
+router.route('/bugs').get(checkAuth, adminController.getAllBugs);
+
+//assign bug
+router.route('/assignbug').post(checkAuth, adminController.assignBug);
+
+//unassign bug
+router.route('/unassignbug').post(checkAuth, adminController.unassignBug);
+
+//delete bug
+router.route('/deletebug').post(checkAuth, adminController.deleteBug);
 
 module.exports = router;
