d51ef5157692db17c6cbeab246123ff27c4b9399
bslzz <bishalkoid@gmail.com>
2020-10-05 21:17:42 +0300

    login form updated

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -21,50 +21,41 @@ const Login = () => {
   };
 
   return (
-    <div className="login_container">
-      <form onSubmit={onSubmit} className="login_form">
-        <div className="login_from_section">
-          <label htmlFor="type" className="login_form_label">
-            Type:
-          </label>
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="type">Type:</label>
           <select
             name="type"
             value={usertype}
             onChange={(e) => setUserType(e.target.value)}
-            className="login_form_select"
           >
             <option value="admin">Admin</option>
             <option value="employee">Employee</option>
             <option value="client">Client</option>
           </select>
         </div>
-        <div className="login_from_section">
-          <label htmlFor="id" className="login_form_label">
-            Id:
-          </label>
+        <div>
+          <label htmlFor="id">Id:</label>
           <input
             type="number"
             name="id"
             value={id}
             onChange={(e) => setId(e.target.value)}
             placeholder="Enter UserId..."
-            className="login_form_input"
           />
         </div>
-        <div className="login_from_section">
-          <label htmlFor="password" className="login_form_label">
-            Password:
-          </label>
+        <div>
+          <label htmlFor="password">Password:</label>
           <input
             type="string"
             name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Enter Password..."
-            className="login_form_input"
           />
         </div>
-        <button className="login_form_button">Login</button>
+        <button>Login</button>
       </form>
     </div>
   );
