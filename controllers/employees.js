2d55f2b75e4a0b84cc5551231dc3d94be226ec3e
bslzz <bishalkoid@gmail.com>
2020-10-06 17:53:46 +0300

    added employees bug schemas in controllers

--- controllers/employees.js
+++ controllers/employees.js
@@ -59,4 +59,68 @@ module.exports = {
       });
     }
   },
+
+  // GET ASSIGNED BUG
+  getAssignedBug: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const bug = await Bug.find({ working_emp_id: id });
+
+      return res.status(200).json({
+        success: true,
+        data: bug,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        error: err,
+      });
+    }
+  },
+
+  //GIVE SOLUTION
+  giveSolution: async (req, res) => {
+    try {
+      const { bug_id, solution } = req.body;
+
+      const token = req.header('Authorization');
+      const decoded = jwt.verify(token, process.env.JWT_KEY);
+      data = decoded;
+
+      if (data.type === 'employee') {
+        const bug = await Bug.find({ bug_id: bug_id });
+
+        if (bug.length > 0) {
+          const filter = { bug_id: bug_id };
+          const update = {
+            solution: solution,
+            bug_status: 'solution is given',
+          };
+
+          const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
+
+          return res.status(200).json({
+            success: true,
+            data: doc,
+          });
+        } else {
+          return res.status(404).json({
+            success: false,
+            message: 'bug not found',
+          });
+        }
+      } else {
+        return res.status(401).json({
+          success: false,
+          message: 'unauthorized',
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        error: err,
+      });
+    }
+  },
 };
