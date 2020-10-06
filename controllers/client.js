85492d19bbbc0b28eeb4d7aeb330f4c6be0cc46e
bslzz <bishalkoid@gmail.com>
2020-10-06 19:05:54 +0300

    modified controllers files for working functionality

--- controllers/client.js
+++ controllers/client.js
@@ -1,254 +1,205 @@
 const Client = require('../models/Client');
+const Bug = require('../models/Bug');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
-module.exports = {
-  //get Clients
-  getClients: async (req, res) => {
-    try {
-      const clients = await Client.find({});
+exports.getClients = async (req, res) => {
+  try {
+    const clients = await Client.find({});
+
+    return res.status(200).json({
+      success: true,
+      data: clients,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      error: err,
+    });
+  }
+};
+
+exports.reportBug = async (req, res) => {
+  try {
+    const { client_id, client_username, project, bug } = req.body;
+
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    data = decoded;
+
+    if (data.type === 'client') {
+      const new_bug = await new Bug({
+        client_id,
+        client_username,
+        project,
+        bug,
+      });
+
+      const result = await new_bug.save();
 
       return res.status(200).json({
         success: true,
-        data: clients,
+        data: new_bug,
       });
-    } catch (err) {
-      return res.status(500).json({
-        status: false,
-        error: err.message,
-      });
-    }
-  },
-
-  // register client
-
-  clientRegister: async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      //validation
-      if (!username || !password)
-        return res.status(422).json({ msg: 'Username and Password required' });
-      if (password.length < 5)
-        return res
-          .status(422)
-          .json({ msg: 'Password must be at least 5 characters long' });
-
-      //finding if the user is stored in the DB
-      const existingUser = await Client.findOne({ username });
-      if (existingUser)
-        return res.status(422).json({ msg: 'User already exists' });
-
-      //hash password
-      const hash = await bcrypt.hash(password, 10);
-
-      const new_user = await new Client({
-        username,
-        password: hash,
-      });
-
-      const result = await new_user.save();
-
-      return res.status(201).json({
-        success: true,
-        type: 'client',
-        data: result,
-      });
-    } catch (err) {
-      return res.status(500).json({
+    } else {
+      return res.status(401).json({
         success: false,
-        error: err.message,
+        message: 'unauthorized only client can create bugs',
       });
     }
-  },
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      error: err,
+    });
+  }
+};
 
-  // REPORT BUG
-  reportBug: async (req, res) => {
-    try {
-      const { client_id, client_username, project, bug } = req.body;
+exports.confirmsolution = async (req, res) => {
+  try {
+    const { bug_id } = req.body;
+    const bug = await Bug.find({ bug_id: bug_id });
+    name = bug[0].working_emp_name;
 
-      const token = req.header('Authorization');
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
-      data = decoded;
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    data = decoded;
 
-      if (data.type === 'client') {
-        const new_bug = await new Bug({
-          client_id,
-          client_username,
-          project,
-          bug,
-        });
+    if (data.type === 'client') {
+      if (bug.length > 0) {
+        const filter = { bug_id: bug_id };
+        const update = {
+          working_emp_id: 0,
+          working_emp_name: `solved by ${name}`,
+          bug_status: 'solved',
+        };
 
-        const result = await new_bug.save();
+        const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
 
         return res.status(200).json({
           success: true,
-          data: new_bug,
+          data: doc,
         });
       } else {
-        return res.status(401).json({
+        return res.status(404).json({
           success: false,
-          message: 'unauthorized only client can create bugs',
+          message: 'bug not found',
         });
       }
-    } catch (err) {
-      return res.status(500).json({
-        status: false,
-        error: err.message,
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: 'unauthorized',
       });
     }
-  },
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      error: err,
+    });
+  }
+};
 
-  // CONFIRM SOLUTION
+exports.denysoluton = async (req, res) => {
+  try {
+    const { bug_id } = req.body;
+    const bug = await Bug.find({ bug_id: bug_id });
 
-  confirmsolution: async (req, res) => {
-    try {
-      const { bug_id } = req.body;
-      const bug = await Bug.find({ bug_id: bug_id });
-      name = bug[0].working_emp_name;
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    data = decoded;
 
-      const token = req.header('Authorization');
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
-      data = decoded;
+    if (data.type === 'client') {
+      if (bug.length > 0) {
+        const filter = { bug_id: bug_id };
+        const update = {
+          bug_status: 'solution not working',
+        };
 
-      if (data.type === 'client') {
-        if (bug.length > 0) {
-          const filter = { bug_id: bug_id };
-          const update = {
-            working_emp_id: 0,
-            working_emp_name: `solved by ${name}`,
-            bug_status: 'solved',
-          };
+        const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
 
-          const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
+        return res.status(200).json({
+          success: true,
+          data: doc,
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: 'bug not found',
+        });
+      }
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: 'unauthorized',
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      error: err,
+    });
+  }
+};
+
+exports.deleteBug = async (req, res) => {
+  try {
+    const { bug_id } = req.body;
+
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    data = decoded;
+
+    const bug = await Bug.find({ bug_id: bug_id });
+
+    if (data.type === 'client') {
+      if (bug.length > 0) {
+        if (data.id === bug[0].client_id) {
+          await Bug.remove({ bug_id: bug_id });
 
           return res.status(200).json({
             success: true,
-            data: doc,
+            message: 'bug has been deleted successfully',
           });
         } else {
-          return res.status(404).json({
+          return res.status(401).json({
             success: false,
-            message: 'bug not found',
+            message: 'you do not have authority to delete this bug',
           });
         }
       } else {
-        return res.status(401).json({
+        return res.status(404).json({
           success: false,
-          message: 'unauthorized',
+          message: 'no bug found',
         });
       }
-    } catch (err) {
-      return res.status(500).json({
-        status: false,
-        error: err.message,
-      });
-    }
-  },
-
-  //DENY SOLUTION
-  denysoluton: async (req, res) => {
-    try {
-      const { bug_id } = req.body;
-      const bug = await Bug.find({ bug_id: bug_id });
-
-      const token = req.header('Authorization');
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
-      data = decoded;
-
-      if (data.type === 'client') {
-        if (bug.length > 0) {
-          const filter = { bug_id: bug_id };
-          const update = {
-            bug_status: 'solution not working',
-          };
-
-          const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
-
-          return res.status(200).json({
-            success: true,
-            data: doc,
-          });
-        } else {
-          return res.status(404).json({
-            success: false,
-            message: 'bug not found',
-          });
-        }
-      } else {
-        return res.status(401).json({
-          success: false,
-          message: 'unauthorized',
-        });
-      }
-    } catch (err) {
-      return res.status(500).json({
-        status: false,
-        error: err.message,
-      });
-    }
-  },
-
-  //DELETE BUG
-  deleteBug: async (req, res) => {
-    try {
-      const { bug_id } = req.body;
-
-      const token = req.header('Authorization');
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
-      data = decoded;
-
-      const bug = await Bug.find({ bug_id: bug_id });
-
-      if (data.type === 'client') {
-        if (bug.length > 0) {
-          if (data.id === bug[0].client_id) {
-            await Bug.remove({ bug_id: bug_id });
-
-            return res.status(200).json({
-              success: true,
-              message: 'bug has been deleted successfully',
-            });
-          } else {
-            return res.status(401).json({
-              success: false,
-              message: 'you do not have authority to delete this bug',
-            });
-          }
-        } else {
-          return res.status(404).json({
-            success: false,
-            message: 'no bug found',
-          });
-        }
-      } else {
-        return res.status(401).json({
-          success: false,
-          message: 'unauthorized',
-        });
-      }
-    } catch (err) {
-      res.status(500).json({
+    } else {
+      return res.status(401).json({
         success: false,
-        error: err.message,
+        message: 'unauthorized',
       });
     }
-  },
-  // GET BUG STATUS
-  getBugStatus: async (req, res) => {
-    try {
-      const { id } = req.params;
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
+};
 
-      const bug = await Bug.find({ client_id: id });
+exports.getBugStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-      return res.status(200).json({
-        success: true,
-        data: bug,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        success: false,
-        error: err.message,
-      });
-    }
-  },
+    const bug = await Bug.find({ client_id: id });
+
+    return res.status(200).json({
+      success: true,
+      data: bug,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
 };
--- controllers/employees.js
+++ controllers/employees.js
@@ -1,126 +1,82 @@
 const Employee = require('../models/Employee');
+const Bug = require('../models/Bug');
+
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
-module.exports = {
-  //get Employee
-  getEmployee: async (req, res) => {
-    try {
-      const employees = await Employee.find({});
+exports.getEmployee = async (req, res) => {
+  try {
+    const employees = await Employee.find({});
 
-      return res.status(200).json({
-        success: true,
-        data: employees,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        status: false,
-        error: err.message,
-      });
-    }
-  },
+    return res.status(200).json({
+      success: true,
+      data: employees,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: false,
+      error: err,
+    });
+  }
+};
 
-  // register employee
-  employeeRegister: async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      //validation
-      if (!username || !password)
-        return res.status(422).json({ msg: 'Username and Password required' });
-      if (password.length < 5)
-        return res
-          .status(422)
-          .json({ msg: 'Password must be at least 5 characters long' });
+exports.getAssignedBug = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-      //finding if the user is stored in the DB
-      const existingUser = await Employee.findOne({ username });
-      if (existingUser)
-        return res.status(422).json({ msg: 'User already exists' });
+    const bug = await Bug.find({ working_emp_id: id });
 
-      //hash password
-      const hash = await bcrypt.hash(password, 10);
+    return res.status(200).json({
+      success: true,
+      data: bug,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
+};
 
-      const new_user = await new Employee({
-        username,
-        password: hash,
-      });
+exports.giveSolution = async (req, res) => {
+  try {
+    const { bug_id, solution } = req.body;
 
-      const result = await new_user.save();
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    data = decoded;
 
-      return res.status(201).json({
-        success: true,
-        type: 'employee',
-        data: result,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        success: false,
-        error: err.message,
-      });
-    }
-  },
+    if (data.type === 'employee') {
+      const bug = await Bug.find({ bug_id: bug_id });
 
-  // GET ASSIGNED BUG
-  getAssignedBug: async (req, res) => {
-    try {
-      const { id } = req.params;
+      if (bug.length > 0) {
+        const filter = { bug_id: bug_id };
+        const update = {
+          solution: solution,
+          bug_status: 'solution is given',
+        };
 
-      const bug = await Bug.find({ working_emp_id: id });
+        const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
 
-      return res.status(200).json({
-        success: true,
-        data: bug,
-      });
-    } catch (err) {
-      return res.status(500).json({
-        success: false,
-        error: err,
-      });
-    }
-  },
-
-  //GIVE SOLUTION
-  giveSolution: async (req, res) => {
-    try {
-      const { bug_id, solution } = req.body;
-
-      const token = req.header('Authorization');
-      const decoded = jwt.verify(token, process.env.JWT_KEY);
-      data = decoded;
-
-      if (data.type === 'employee') {
-        const bug = await Bug.find({ bug_id: bug_id });
-
-        if (bug.length > 0) {
-          const filter = { bug_id: bug_id };
-          const update = {
-            solution: solution,
-            bug_status: 'solution is given',
-          };
-
-          const doc = await Bug.findOneAndUpdate(filter, update, { new: true });
-
-          return res.status(200).json({
-            success: true,
-            data: doc,
-          });
-        } else {
-          return res.status(404).json({
-            success: false,
-            message: 'bug not found',
-          });
-        }
+        return res.status(200).json({
+          success: true,
+          data: doc,
+        });
       } else {
-        return res.status(401).json({
+        return res.status(404).json({
           success: false,
-          message: 'unauthorized',
+          message: 'bug not found',
         });
       }
-    } catch (err) {
-      return res.status(500).json({
+    } else {
+      return res.status(401).json({
         success: false,
-        error: err,
+        message: 'unauthorized',
       });
     }
-  },
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: err,
+    });
+  }
 };
