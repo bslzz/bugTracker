065d9a74742db57ea280aa2ebf978d44359320b8
bslzz <bishalkoid@gmail.com>
2020-10-06 19:06:40 +0300

    auth token updated

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,21 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const checkAuth = (req, res, next) => {
+module.exports = (req, res, next) => {
   try {
-    const token = req.header('Authorization');
-    if (!token)
-      return res.status(401).json({ msg: 'Unauthorized token, Access denied' });
-
+    const token = req.headers.authorization.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_KEY);
-    if (!decoded)
-      return res.status(401).json({ msg: 'Unauthorized User, Access denied' });
     req.userData = decoded;
     next();
   } catch (error) {
-    res.status(500).json({
-      msg: error.message,
+    return res.status(401).json({
+      message: 'Auth failed',
     });
   }
 };
-
-module.exports = checkAuth;
