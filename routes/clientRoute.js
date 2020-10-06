b32bb8fa6fde8353cdb42874ba245d0c71848357
bslzz <bishalkoid@gmail.com>
2020-10-06 17:54:39 +0300

    added controllers routes to their respective fields

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -2,8 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 const clientController = require('../controllers/client');
+const checkAuth = require('../middleware/Auth');
 
+// GET CLIENTS
 router.route('/').get(clientController.getClients);
+
+//REGISTER CLIENT
 router.route('/clientRegister').post(clientController.clientRegister);
+
+// REPORT BUG
+router.route('/report_bug').post(checkAuth, clientController.reportBug);
+
+// DELETE BUG
+router.route('/delete_bug').post(checkAuth, clientController.deleteBug);
+
+//GET BUG STATUS
+router.route('/bugs/:id').get(checkAuth, clientController.getBugStatus);
+
+// CONFIRM SOLUTION
+router
+  .route('/confirmsolution')
+  .post(checkAuth, clientController.confirmsolution);
+
+// DENY SOLUTION / NOT WORKING
+router.route('/notworking').post(checkAuth, clientController.denysoluton);
 
 module.exports = router;
--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -2,8 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const employeeController = require('../controllers/employees');
+const checkAuth = require('../middleware/Auth');
 
+// GET EMPLOYEE
 router.route('/').get(employeeController.getEmployee);
+
+// REGISTER EMPLOYEE
 router.route('/employeeRegister').post(employeeController.employeeRegister);
+
+// GET ASSIGNED BUG
+router
+  .route('/getassignedbug/:id')
+  .get(checkAuth, employeeController.getAssignedBug);
+
+// GIVE SOLUTION
+router.route('/givesolution').post(checkAuth, employeeController.giveSolution);
 
 module.exports = router;
