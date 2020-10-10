a03f7cf53f5968156e95e9c2d5e8b1b743cb7552
bslzz <bishalkoid@gmail.com>
2020-10-10 12:00:57 +0300

    added landing page to the project

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Home = () => {
+  const history = useHistory();
+  const login = () => {
+    history.push('/login');
+  };
+
+  const register = () => {
+    history.push('/register');
+  };
+  return (
+    <>
+      <button onClick={login}>Login</button>
+      <button onClick={register}>Register</button>
+    </>
+  );
+};
+
+export default Home;
--- client/src/routes/Index.js
+++ client/src/routes/Index.js
@@ -5,6 +5,7 @@ import Route from './Route';
 
 import { GlobalContext } from '../context/GlobalState';
 
+import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Admin from '../pages/Admin';
 import Employee from '../pages/Employee';
@@ -19,7 +20,8 @@ export default function Routes() {
 
   return (
     <Switch>
-      <Route path="/" exact component={Login} />
+      <Route path="/" exact component={Home} />
+      <Route path="/login" exact component={Login} />
 
       <Route path="/admin" component={Admin} isPrivate />
 
@@ -28,7 +30,7 @@ export default function Routes() {
       <Route path="/client" component={Client} isPrivate />
 
       {/* redirect user to SignIn page if route does not exist and user is not authenticated */}
-      <Route component={Login} />
+      <Route component={Home} />
     </Switch>
   );
 }
