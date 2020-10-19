77fd08359cddbef6f86f7de3c5622264d3cb8385
flo20 <59807384+flo20@users.noreply.github.com>
2020-10-19 17:00:41 +0300

    Creating landing page draft and navbar

--- client/src/pages/Navbars.js
+++ client/src/pages/Navbars.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbars.scss";
-  
+
 const Navbars = () => {
-    return (
-        <div className="navbar-container">
-            <div>
-                LOGO
-            </div>
-            <button>
-                Login
-            </button>
-            <button>
-                Register as admin
-            </button>
-        </div>
-    );
-}
+    const [click, setClick] = useState(false);
+    
+    // handle navbar hamburger toggle 
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = ()=> setClick(false)
+  return (
+    // <div className="navbar-container">
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        EPIC
+      </Link>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+      </div>
+
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-item">
+          <Link to="/login" className="nav-links">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/register" className="nav-links">
+            Register as Admin
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbars;
