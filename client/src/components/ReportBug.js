3ee0636c168bfe3e1f218c8ac20ca9ae534a7820
bslzz <bishalkoid@gmail.com>
2020-11-03 20:44:05 +0200

    added classname

--- client/src/components/ReportBug.js
+++ client/src/components/ReportBug.js
@@ -23,29 +23,35 @@ export const ReportBug = (props) => {
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="id">Project:</label>
+    <div className="report_bug_container">
+      <form onSubmit={onSubmit} className="report_bug_form">
+        <div className="report_bug_form_section">
+          <label htmlFor="id" className="report_bug_form_label">
+            Project:
+          </label>
           <input
             type="string"
             name="project"
             value={project}
             onChange={(e) => setProject(e.target.value)}
             placeholder="Enter the name of project..."
+            className="report_bug_form_input"
           />
         </div>
-        <div>
-          <label htmlFor="password">Bug:</label>
+        <div className="report_bug_form_section">
+          <label htmlFor="password" className="report_bug_form_label">
+            Bug:
+          </label>
           <input
             type="string"
             name="Bug"
             value={bug}
             onChange={(e) => setBug(e.target.value)}
             placeholder="Enter The bug..."
+            className="report_bug_form_input"
           />
         </div>
-        <button>Report Bug</button>
+        <button className="bug_button bug_button_report">Report Bug</button>
       </form>
     </div>
   );
