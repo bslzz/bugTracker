7502d28a3a26042b9184e58bcc0ef801213e011d
bslzz <bishalkoid@gmail.com>
2020-11-03 21:07:55 +0200

    added classname for css

--- client/src/components/SolvedBug.js
+++ client/src/components/SolvedBug.js
@@ -11,23 +11,23 @@ export const SolvedBug = (props) => {
     <>
       <div className={`bug__container ${className}`}>
         <p>
-          <span className="bug__titles">Client Username:</span>{' '}
+          <span className="bug__titles">Client Username: </span>{' '}
           {bug.client_username}
         </p>
         <p>
-          <span className="bug__titles">Project:</span> {bug.project}
+          <span className="bug__titles">Project: </span> {bug.project}
         </p>
         <p>
-          <span className="bug__titles">Bug:</span> {bug.bug}
+          <span className="bug__titles">Bug: </span> {bug.bug}
         </p>
         <p>
-          <span className="bug__titles">Bug Status:</span> {bug.bug_status}
+          <span className="bug__titles">Bug Status: </span> {bug.bug_status}
         </p>
         <p>
-          <span className="bug__titles">Solution:</span> {bug.solution}
+          <span className="bug__titles">Solution: </span> {bug.solution}
         </p>
         <p>
-          <span className="bug__titles">Working Employee:</span>{' '}
+          <span className="bug__titles">Working Employee: </span>{' '}
           {bug.working_emp_name}
         </p>
         <button className="bug_button bug_button_delete" onClick={onClick}>
