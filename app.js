8979f08fe8482a9647fefa16d5291ab29507cf8a
bslzz <bishalkoid@gmail.com>
2020-11-09 19:12:13 +0200

    edited css and port

--- app.js
+++ app.js
@@ -26,5 +26,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //PORT
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
